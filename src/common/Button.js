951970db824fd87f143ae2bc9d37a7ff19b980ed
yeram-lim <yeramyeye@gmail.com>
2023-02-23 20:26:25 +0900

    😺[CHORE] button component

---
+++ src/common/Button.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const ButtonStyle = styled.div`
+  & {
+    display: flex;
+    align-items: center;
+    text-align: center;
+    justify-content: center;
+    width: ${(props) => props.width};
+    height: 46.51px;
+
+    position: relative;
+    background: linear-gradient(
+        109.78deg,
+        #788eff 37.44%,
+        rgba(255, 255, 255, 0) 196.54%
+      ),
+      linear-gradient(
+        108.83deg,
+        #000000 165.46%,
+        rgba(255, 255, 255, 0) 210.88%
+      );
+    border-radius: 9px;
+    cursor: pointer;
+
+    > a {
+      text-decoration: none;
+      font-family: "Saira";
+      font-style: normal;
+      font-weight: 400;
+      font-size: 20px;
+      line-height: 124.19%;
+      color: #ffffff;
+    }
+  }
+`;
+
+const Button = ({ text, width = "100%", link = null }) => {
+  return (
+    <>
+      <ButtonStyle width={width}>
+        {link ? <Link to={`${link}`}>{text}</Link> : { text }}
+      </ButtonStyle>
+    </>
+  );
+};
+
+export default Button;
