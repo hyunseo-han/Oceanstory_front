e5c77f62a09d2b4f84a05490854f74cde5241b87
yeram-lim <yeramyeye@gmail.com>
2023-02-24 21:36:32 +0900

    🍭[STYLE] account container styled component

---
+++ src/common/AccountStyle.js
@@ -0,0 +1,55 @@
+import Styled from "styled-components";
+
+export const AccountContainer = Styled.section`
+&{
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  div.title {
+    position: relative;
+    height: 59px;
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 133.19%;
+    display: flex;
+    align-items: center;
+    text-align: center;
+    justify-content: center;
+    letter-spacing: 0.22em;
+    color: #ffffff;
+    backdrop-filter: blur(2px);
+  }
+
+  div.input-wrap {
+    position: relative;
+    align-items: center;
+    text-align: center;
+    justify-content: space-between;
+    margin-bottom: 3rem;
+
+    div.text {
+      position: relative;
+      width: 228px;
+      height: 48px;
+      font-family: "Roboto";
+      font-style: normal;
+      font-weight: 400;
+      font-size: 12px;
+      line-height: 124.19%;
+      display: flex;
+      align-items: center;
+      color: #7e7e7e;
+      margin: auto; 
+    }
+
+    input {
+
+    }
+  }
+}
+`;
