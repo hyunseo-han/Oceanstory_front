5541f2f989366b079526fe3c453c35a54a1a5192
hyunseo-han <80339766+hyunseo-han@users.noreply.github.com>
2023-02-25 19:09:50 +0900

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -67,18 +67,7 @@ function Main() {
           gap: "3rem",
         }}
       >
-        <div class="bubble bubble--1"></div>
-        <div class="bubble bubble--2"></div>
-        <div class="bubble bubble--3"></div>
-        <div class="bubble bubble--4"></div>
-        <div class="bubble bubble--5"></div>
-        <div class="bubble bubble--6"></div>
-        <div class="bubble bubble--7"></div>
-        <div class="bubble bubble--8"></div>
-        <div class="bubble bubble--9"></div>
-        <div class="bubble bubble--10"></div>
-        <div class="bubble bubble--11"></div>
-        <div class="bubble bubble--12"></div>
+       
         <Title>
           OCEAN
           <br /> STORY
